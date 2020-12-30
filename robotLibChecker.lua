01673d297063832a9a9812f1ed60999fd986f8b0
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2020-12-30 23:50:39 +0800

    Adding robotLibChecker and robotCommon

---
+++ robotLibChecker.lua
@@ -0,0 +1,13 @@
+local fs = require("filesystem")
+local component = require("component")
+local counter = 0
+
+print("Running library checker for robots...")
+
+print("Checking for missing files on /home/robotlib directory...")
+
+if(not fs.exists("/home/robotlib/robotCommon.lua")) then
+    print("Missing robotCommon.lua...")
+    counter = counter + 1
+end
+    print("Total of ".. counter .. "missing file detected!")
