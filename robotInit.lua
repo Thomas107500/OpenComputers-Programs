672cf136ddfd9b0441e5bd1ce072bb69e34b2f45
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2021-01-01 19:28:39 +0800

    Update robotInit.lua

--- robotInit.lua
+++ robotInit.lua
@@ -8,10 +8,15 @@ local z = commonlib.input("Please input the current Z coordinate: ")
 local facing = commonlib.input("Please input the current facing(1:north,2:east,3:south,4:west): ")
 
 function init() 
+    local f
     fs.makeDirectory("/home/data/coords.lua")
-    local file = io.open("/home/data/coords.lua","w")
-    file.write(x.. "\n" .. y .. "\n" .. z .."\n" .. facing)
-    file.close()
+    local status, err = pcall(function() f = io.open("/home/data/coords.lua","w") end)
+    if (status) then
+        f.write(x.. "\n" .. y .. "\n" .. z .."\n" .. facing)
+        f.close()
+    else
+        print("An error occurred when writing: ".. err)
+    end
 end
 
 local status, err = pcall(init())
