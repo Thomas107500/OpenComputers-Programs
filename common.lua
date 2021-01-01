f3e577853647af5a983efc078238f81fc176ba65
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2021-01-01 19:33:02 +0800

    Update libraries syntax

--- common.lua
+++ common.lua
@@ -1,9 +1,10 @@
+local common = {}
 
-
-
-function input(String_prompt)
+function common.input(String_prompt)
     io.write(String_prompt)
     local result = io.read()
     io.write("\n")
     return result
 end
+
+return common
