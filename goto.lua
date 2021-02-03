9c0b225463c8b31484bd9d3d406e326b6f0579ce
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2021-02-03 19:24:02 +0800

    Fix goto

--- goto.lua
+++ goto.lua
@@ -4,5 +4,5 @@ local commonlib = require("common")
 local Vector3_current,facing = robotcommon.getCoord()
 local Vector3_dest = {tonumber(commonlib.input("X: ")),tonumber(commonlib.input("Y: ")),tonumber(commonlib.input("Z: "))}
 
-Vector3_current,facing = robotcommon.move(Vector3_dest,Vector3_current,facing,false)
+Vector3_current,facing = robotcommon.move(Vector3_current,Vector3_dest,facing,false)
 robotcommon.storeCoord(Vector3_current,facing)
