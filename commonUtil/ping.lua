7d4f91a66705d8526ded8d460f1083c9d0d02628
Thomas107500 <49201909+Thomas107500@users.noreply.github.com>
2021-11-12 23:14:47 +0800

    Create ping.lua

---
+++ commonUtil/ping.lua
@@ -0,0 +1,17 @@
+--Requirement to run: Internet Card
+
+
+local internet = require("internet")
+
+print("Checking availability of https://ocwebapi.thomas107500.repl.co/api/...")
+
+local content = ""
+local result = internet.request("https://ocwebapi.thomas107500.repl.co/api/ping")
+
+for chunk in result do content = content..chunk end
+
+if(content == "pong") then
+    print("pong received")
+else    
+    print("Failed. returned content: " .. content)
+end
